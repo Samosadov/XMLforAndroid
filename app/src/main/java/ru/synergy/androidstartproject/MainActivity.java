5bcaa6464d03787808397a901fead1f7e4fbecd4
Samosadov <for-spam2@yandex.ru>
2023-01-15 20:07:10 +0300

    Very strange example: nothing clear

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -12,11 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        ConstraintLayout constraintLayout = 
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
         TextView textView = new TextView(this);
         textView.setText("Просто пример.");
-        textView.setTextSize(22);
-        setContentView(textView);
+        textView.setTextSize(26);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        textView.setLayoutParams(layoutParams);
+        constraintLayout.addView(textView);
+        setContentView(constraintLayout);
 
     }
 }

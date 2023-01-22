a69f1fd306fcd678d8ed08e1784cd44521d94392
Samosadov <for-spam2@yandex.ru>
2023-01-22 18:32:19 +0300

    Power of Constraints

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -14,28 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        TextView textView = (TextView) findViewById(R.id.t1);
-        textView.setText("Hard Edit");
-        textView.setTextColor(Color.BLUE);
-//        setContentView(R.layout.second_layout);
-//        TextView textView = (TextView) findViewById(R.id.header);
-//        textView.setText("Просто пример...");
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//        textView.setText("Просто пример.");
-//        textView.setTextSize(26);
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-//
-//        textView.setLayoutParams(layoutParams);
-//        constraintLayout.addView(textView);
-//        setContentView(constraintLayout);
+
 
     }
 }
